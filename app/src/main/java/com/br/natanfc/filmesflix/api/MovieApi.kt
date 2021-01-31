651f9f6010015bb7b3427a89c9a1b32eacb30c77
natanbrito <natanfelipe@gmail.com>
2021-01-31 08:26:32 -0300

    Aula 3 - setup webservice

---
+++ app/src/main/java/com/br/natanfc/filmesflix/api/MovieApi.kt
@@ -0,0 +1,11 @@
+package com.br.natanfc.filmesflix.api
+
+import com.br.natanfc.filmesflix.model.Movie
+import retrofit2.http.GET
+
+interface MovieApi {
+
+    @GET("natanfelipe/FilmesFlixJson/master/moviesList")
+    fun getAllMovies(): List<Movie>
+
+}
---
+++ app/src/main/java/com/br/natanfc/filmesflix/api/MovieRestApiTask.kt
@@ -0,0 +1,19 @@
+package com.br.natanfc.filmesflix.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MovieRestApiTask {
+
+    companion object {
+        const val BASE_URL = "https://raw.githubusercontent.com/"
+    }
+
+    private fun movieProvider(): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun retrofitApi(): MovieApi = movieProvider().create(MovieApi::class.java)
+
+}
